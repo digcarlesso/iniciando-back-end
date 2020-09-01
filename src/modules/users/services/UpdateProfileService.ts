5a298ed6225bc58b954e28b63b4df3dbf169e419
Diogo Bedin Carlesso <dig.skull@gmail.com>
2020-08-31 22:26:10 -0300

    Fixed HashProvider injection

--- src/modules/users/services/UpdateProfileService.ts
+++ src/modules/users/services/UpdateProfileService.ts
@@ -19,7 +19,7 @@ class UpdateProfileService {
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
 
-    @inject('StorageProvider')
+    @inject('HashProvider')
     private hashProvider: IHashProvider,
   ) {}
 
